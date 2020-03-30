a0a5ba8b12d204d7e93be6d50737bae535bbb494
Balazs Nyirfa <38496187+anymus0@users.noreply.github.com>
2020-03-30 11:59:05 +0200

    add npmBoilerPlate generator shell script

---
+++ npmgen.sh
@@ -0,0 +1,10 @@
+npm init -y
+mkdir src
+mkdir src/client
+mkdir src/client/js
+mkdir src/client/scss
+touch src/client/js/main.js && touch src/client/index.html
+mkdir src/server
+touch src/server/main.js
+npm install express
+npm install jquery && npm install bootstrap && npm install body-parser && npm install parcel-bundler --save-dev && npm install nodemon --save-dev
